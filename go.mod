e769628227dbd3bbc97ff5f7bd12985063fb89e9
techrook <mondayitohowo3@gmail.com>
2024-08-10 00:05:31 +0100

    stated server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/techrook/23_RSS_AGGREGATOR
 
 go 1.22.5
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-	
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 
 	if  portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	router:= chi.NewRouter()
+
+	server:= &http.Server{
+		Handler: router,
+		Addr: ":" + portString,
+	}
+	log.Printf("Server starting on port %v", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("Port:", portString)
